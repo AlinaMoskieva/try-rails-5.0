89ce16c73ca82432d5c62d9b12658bb2b2def2ff
Alina Moskieva <alina.moskieva@flatstack.com>
2020-02-09 22:39:06 +0100

    Introdue Notification model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191230155841) do
+ActiveRecord::Schema.define(version: 20200209213823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notifications", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description",          null: false
